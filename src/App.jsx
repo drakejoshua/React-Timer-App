29d8603446eff4d7384a985ac3b827a41f57b263
Joshua Mabawonku <drakejoshua086@gmail.com>
2024-05-23 14:41:05 +0100

    feat: app's markup and jsx structure
    - finished working on the app's markup and structure for jsx
    - added resource link to add google's material symbols from
      their website (https://fonts.google.com/icons)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,120 @@
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1>
-        hello
-      </h1>
+    // app container
+    <div className='app'>
+
+      {/* timer's clock */}
+      <div className="clock">
+
+        <svg height="100" width="100" 
+            xmlns="http://www.w3.org/2000/svg" className='clock-stroke'>
+
+          <circle r="45" cx="50" cy="50" 
+              stroke="green" strokeWidth="3" fill="white"/>
+
+          Sorry, your browser does not support inline SVG.  
+        </svg> 
+
+        <div className="clock-hands">
+          <div className="clock-hand-1"></div>
+          <div className="clock-hand-2"></div>
+        </div>
+      </div>
+
+
+      {/* timer's ctn */}
+      <div className="timer">
+
+        {/* timer's inputs */}
+        <div className="timer-input-ctn">
+          <div className="timer-input">
+
+            <span className="material-symbols-outlined input-increment-control">
+              keyboard_arrow_up
+            </span>
+
+            <span className='input-value'>
+              23
+            </span>
+            
+            <span className="material-symbols-outlined input-decrement-control">
+              keyboard_arrow_down
+            </span>
+          </div>
+
+          <div className="timer-input">
+
+            <span className="material-symbols-outlined input-increment-control">
+              keyboard_arrow_up
+            </span>
+
+            <span className='input-value'>
+              23
+            </span>
+            
+            <span className="material-symbols-outlined input-decrement-control">
+              keyboard_arrow_down
+            </span>
+          </div>
+
+          <div className="timer-input">
+
+            <span className="material-symbols-outlined input-increment-control">
+              keyboard_arrow_up
+            </span>
+
+            <span className='input-value'>
+              23
+            </span>
+            
+            <span className="material-symbols-outlined input-decrement-control">
+              keyboard_arrow_down
+            </span>
+          </div>
+
+          <div className="timer-input">
+
+            <span className="material-symbols-outlined input-increment-control">
+              keyboard_arrow_up
+            </span>
+
+            <span className='input-value'>
+              23
+            </span>
+            
+            <span className="material-symbols-outlined input-decrement-control">
+              keyboard_arrow_down
+            </span>
+          </div>
+        </div>
+
+        {/* timer's status text */}
+        <p>
+          timer running
+        </p>
+
+        {/* timer's controls */}
+        <div className="timer-controls">
+          <button className='reset-btn'>
+            <span className="material-symbols-outlined">
+              replay
+            </span>
+
+            <span>reset</span>
+          </button>
+
+          <button className='stop-btn'>
+            <span className="material-symbols-outlined">
+              stop_circle
+            </span>
+
+            <span>stop</span>
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
